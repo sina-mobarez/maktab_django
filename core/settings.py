bc0fe119aa75abef5f5f9e4377dba5bbebd64b21
Parsa Partovi <pparsapartovi13@gmail.com>
2026-02-05 10:23:22 +0330

    what about now

--- core/settings.py
+++ core/settings.py
@@ -74,15 +74,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
+
 DATABASES = {
-        'default': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'your_db_name',
         'USER': 'your_db_user',
         'PASSWORD': 'your_db_password',
         'HOST': 'localhost',
         'PORT': '5432',
-        'hello':'ds'
     }
 }
 
